9942b51d9a571048b95e1e625e4599e32736dcd3
luxufan <luxufan981014@gmail.com>
2024-03-22 00:08:19 +0800

    replace Instruction::classof

--- llvm/include/llvm/IR/Instruction.h
+++ llvm/include/llvm/IR/Instruction.h
@@ -898,7 +898,7 @@ public:
 
   /// Methods for support type inquiry through isa, cast, and dyn_cast:
   static bool classof(const Value *V) {
-    return V->getValueID() >= Value::InstructionVal;
+    return dynamic_cast<const Instruction *>(V);
   }
 
   //----------------------------------------------------------------------
