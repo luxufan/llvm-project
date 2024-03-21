ac00d1963767c4e3a14126da67868cd8685399db
luxufan <luxufan981014@gmail.com>
2024-03-22 02:07:12 +0800

    fix cmake error

--- llvm/cmake/modules/AddLLVM.cmake
+++ llvm/cmake/modules/AddLLVM.cmake
@@ -1534,7 +1534,7 @@ macro(add_custom_linker_flags name)
   if (LLVM_${name}_LINKER_FLAGS)
     separate_arguments(flags UNIX_COMMAND "${LLVM_${name}_LINKER_FLAGS}")
     message(DEBUG "Applying ${flags} to ${name}")
-    target_link_options(${name} PRIVATE ${name})
+    target_link_options(${name} PRIVATE ${flags})
   endif()
 endmacro()
 
